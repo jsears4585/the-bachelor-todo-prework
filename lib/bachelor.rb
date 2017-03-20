1c49ef31cd9a28c6b98c6cbcfac94d3659985cec
Jason Sears <jsears4585@gmail.com>
2017-03-20 02:37:00 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,79 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_data, contestants| # hash
+    if season == season_data
+      contestants.each do |contestant| # array
+        contestant.each do |attribute, value| # hash
+          if attribute == "status"
+            if value == "Winner"
+              return contestant["name"].split[0]
+            end
+          end
+        end
+      end
+    end
+  end
 end
 
+
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_data, contestants| # hash
+    contestants.each do |contestant| # array
+      contestant.each do |attribute, value| # hash
+        if attribute == "occupation"
+          if value == occupation
+            return contestant["name"]
+          end
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season_data, contestants| # hash
+    contestants.each do |contestant| # array
+      contestant.each do |attribute, value| # hash
+        if attribute == "hometown"
+          if value == hometown
+            count += 1
+          end
+        end
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_data, contestants| # hash
+    contestants.each do |contestant| # array
+      contestant.each do |attribute, value| # hash
+        if attribute == "hometown"
+          if value == hometown
+            return contestant["occupation"]
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_array = []
+  data.each do |season_data, contestants| # hash
+    if season == season_data
+      contestants.each do |contestant| # array
+        contestant.each do |attribute, value| # hash
+          if attribute == "age"
+            age_array << value
+          end
+        end
+      end
+    end
+  end
+  num_arr = age_array.map(&:to_i)
+  (num_arr.inject(0.0) {|sum, el| sum + el} / num_arr.size).round
 end
